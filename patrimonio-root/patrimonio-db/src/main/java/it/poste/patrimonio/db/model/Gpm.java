1c78c44df8f66f80f5187c4b85cdcc25b9957df1
MASTER\santamariag <giuseppe.santamaria@tasgroup.eu>
2024-01-16 11:19:01 +0100

    Index creation for Gpm and Foe

--- patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/model/Gpm.java
+++ patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/model/Gpm.java
@@ -5,12 +5,19 @@ import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
 @Document(collection = "gpms")
 @Data
+@CompoundIndexes({
+    @CompoundIndex(name = "idx-ndg_prod_idprod", def = "{'ndg' : 1, 'patrimonioOld.posizioni.cstrfin': 1, 'patrimonioOld.posizioni.idProd': 1}"),
+    @CompoundIndex(name = "idx-fiscalcode_prod_idprod", def = "{'fiscalCode' : 1, 'patrimonioOld.posizioni.cstrfin': 1, 'patrimonioOld.posizioni.idProd': 1}"),
+    @CompoundIndex(name = "idx-rapporto_prod_idprod", def = "{'rapporto' : 1, 'patrimonioOld.posizioni.cstrfin': 1, 'patrimonioOld.posizioni.idProd': 1}")
+})
 public class Gpm {
 	
 	@Id
