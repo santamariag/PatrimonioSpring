26226205d8f10c552d22d4d308df92792c73732a
desimonea1 <alessandro.desimone@tasgroup.eu>
2024-01-31 16:56:31 +0100

    creata classe comune tra gpm e foe

--- patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/model/CommonData.java
+++ patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/model/CommonData.java
@@ -9,12 +9,32 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
-@Document(collection = "foes")
 @Data
 @CompoundIndexes({
     @CompoundIndex(name = "idx-ndg_prod_idprod", def = "{'ndg' : 1, 'patrimonioOld.posizioni.cstrfin': 1, 'patrimonioOld.posizioni.idProd': 1}"),
     @CompoundIndex(name = "idx-rapporto_prod_idprod", def = "{'rapporto' : 1, 'patrimonioOld.posizioni.cstrfin': 1, 'patrimonioOld.posizioni.idProd': 1}")
 })
-public class Foe extends CommonData {
+public class CommonData {
+	
+	@Id
+	private String ndg;
+	@Version
+	private Long version;
+	
+	private String fiscalCode;
+	
+	private String rapporto; //concatenazione di filiale agenzia numero e rubrica (vedi Deposit)
+
+	private Deposit deposit;
+	
+	private String internalCustomerCode;
+	
+	private String customerDescr;
+	
+	private ExternalKeys externalKeys; // TODO forse non servono
+	
+	private PatrimonioOld patrimonioOld;
+	
+	private Patrimonio patrimonio;
 	
 }
--- patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/model/Gpm.java
+++ patrimonio-root/patrimonio-db/src/main/java/it/poste/patrimonio/db/model/Gpm.java
@@ -15,27 +15,7 @@ import lombok.Data;
     @CompoundIndex(name = "idx-ndg_prod_idprod", def = "{'ndg' : 1, 'patrimonioOld.posizioni.cstrfin': 1, 'patrimonioOld.posizioni.idProd': 1}"),
     @CompoundIndex(name = "idx-fiscalcode_prod_idprod", def = "{'fiscalCode' : 1, 'patrimonioOld.posizioni.cstrfin': 1, 'patrimonioOld.posizioni.idProd': 1}")
 })
-public class Gpm {
+public class Gpm extends CommonData{
 	
-	@Id
-	private String ndg;
-	@Version
-	private Long version;
-	
-	private String fiscalCode;
-	
-	private String rapporto; //concatenazione di filiale agenzia numero e rubrica (vedi Deposit)
 
-	private Deposit deposit;
-	
-	private String internalCustomerCode;
-	
-	private String customerDescr;
-	
-	private ExternalKeys externalKeys; // TODO forse non servono
-	
-	private PatrimonioOld patrimonioOld;
-	
-	private Patrimonio patrimonio;
-	
 }
