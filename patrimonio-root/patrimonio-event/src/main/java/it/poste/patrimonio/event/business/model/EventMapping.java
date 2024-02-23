3068c54289ae9a2a13c5ce70061775fce1296f3d
salvatore.pellitteri <salvatore.pellitteri@wedare.it>
2024-02-23 10:24:13 +0100

    Lista Eventi

--- patrimonio-root/patrimonio-event/src/main/java/it/poste/patrimonio/event/business/model/EventMapping.java
+++ patrimonio-root/patrimonio-event/src/main/java/it/poste/patrimonio/event/business/model/EventMapping.java
@@ -6,17 +6,17 @@ public class EventMapping {
         "business-event.gpm-foe.cancellation-from-backoffice"      + ":it.poste.patrimonio.event.business.impl.gpmfoe.CancellationFromBackOffice,"+
         "business-event.gpm-foe.cancellation-from-client"          + ":it.poste.patrimonio.event.business.impl.gpmfoe.CancellationFromClient, " +
         "business-event.gpm-foe.cancellation-from-product-company" + ":it.poste.patrimonio.event.business.impl.gpmfoe.CancellationFromProductCompany, " +
-        "business-event.gpm-foe.fund-order-creation:it.poste.patrimonio.event.business.impl.gpmfoe.FundOrderCreation, " +
-        "business-event.gpm-foe.master-data-creation:it.poste.patrimonio.event.business.impl.gpmfoe.MasterDataCreation, " +
-        "business-event.gpm-foe.master-data-delete:it.poste.patrimonio.event.business.impl.gpmfoe.MasterDataDelete, " +
-        "business-event.gpm-foe.master-data-lock:it.poste.patrimonio.event.business.impl.gpmfoe.MasterDataLock, " +
-        "business-event.gpm-foe.pac-creation:it.poste.patrimonio.event.business.impl.gpmfoe.PacCreation, " +
-        "business-event.gpm-foe.pac-delete:it.poste.patrimonio.event.business.impl.gpmfoe.PacDelete, " +
-        "business-event.gpm-foe.refund-feedback:it.poste.patrimonio.event.business.impl.gpmfoe.RefundFeedback, " +
-        "business-event.gpm-foe.refund-order:it.poste.patrimonio.event.business.impl.gpmfoe.RefundOrder, " +
-        "business-event.gpm-foe.subscription-feedback:it.poste.patrimonio.event.business.impl.gpmfoe.SubscriptionFeedback, " +
-        "business-event.gpm-foe.subscription-order:it.poste.patrimonio.event.business.impl.gpmfoe.SubscriptionOrder";
+        "business-event.gpm-foe.fund-order-creation"               + ":it.poste.patrimonio.event.business.impl.gpmfoe.FundOrderCreation, " +
+        "business-event.gpm-foe.master-data-creation"              + ":it.poste.patrimonio.event.business.impl.gpmfoe.MasterDataCreation, " +
+        "business-event.gpm-foe.master-data-delete"                + ":it.poste.patrimonio.event.business.impl.gpmfoe.MasterDataDelete, " +
+        "business-event.gpm-foe.master-data-lock"                  + ":it.poste.patrimonio.event.business.impl.gpmfoe.MasterDataLock, " +
+        "business-event.gpm-foe.pac-creation"                      + ":it.poste.patrimonio.event.business.impl.gpmfoe.PacCreation, " +
+        "business-event.gpm-foe.pac-delete"                        + ":it.poste.patrimonio.event.business.impl.gpmfoe.PacDelete, " +
+        "business-event.gpm-foe.refund-feedback"                   + ":it.poste.patrimonio.event.business.impl.gpmfoe.RefundFeedback, " +
+        "business-event.gpm-foe.refund-order"                      + ":it.poste.patrimonio.event.business.impl.gpmfoe.RefundOrder, " +
+        "business-event.gpm-foe.subscription-feedback"             + ":it.poste.patrimonio.event.business.impl.gpmfoe.SubscriptionFeedback, " +
+        "business-event.gpm-foe.subscription-order"                + ":it.poste.patrimonio.event.business.impl.gpmfoe.SubscriptionOrder";
 
 
-    public static final String TITOLI_TYPE_MAPPINGS = "business-event.titoli.TestTitoliEvent:it.poste.patrimonio.event.business.impl.titoli.TestTitoliEvent";
+    public static final String TITOLI_TYPE_MAPPINGS = "business-event.titoli.TestTitoliEvent" + ":it.poste.patrimonio.event.business.impl.titoli.TestTitoliEvent";
 }
